4cacf4d9897f1691407ef691c0ec20025154e9e7
sunny-zerror <sunny.zerror@gmail.com>
2025-04-19 14:01:45 +0530

    seo added

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,19 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link rel="stylesheet" href="" />{" "} */}
-        {/* <script
-          async
-          src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@0.2.28/bundled/lenis.js"
-        ></script> */}
-        <link
-          rel="stylesheet"
-          href="https://use.typekit.net/izt7oyh.css"
-        ></link>
-        {/* <link
-          rel="stylesheet"
-          href="https://use.typekit.net/smz6nuo.css"
-        ></link> */}
+        <title>RAS-A Goodrich Company</title>
+        <link rel="shortcut icon" href="https://goodrich.co/wp-content/uploads/2022/03/Goodrich-Favicon.svg" type="image/x-icon" />
+        <meta name="description" content="RAS, a Goodrich company, focuses on supply chain continuity through a well connected agency network, building on delivering solutions that are responsible, safe, and sustainable." />
+        <meta name="keywords" content="RAS, Goodrich, supply chain, continuity, agency network, logistics, responsible solutions, safe solutions, sustainable solutions, supply chain management, logistics company" />
+        <meta name="author" content="RAS Team" />
+        <meta property="og:title" content="RAS, a Goodrich company, focuses on supply chain continuity through a well connected agency network, building on delivering solutions that are responsible, safe, and sustainable." />
+        <meta property="og:description" content="RAS, a Goodrich company, focuses on supply chain continuity through a well connected agency network, building on delivering solutions that are responsible, safe, and sustainable." />
+        <meta property="og:image" content="/images/twitter_image.png" />
+        <meta property="og:url" content="https://ras-seven.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="RAS, a Goodrich company, focuses on supply chain continuity through a well connected agency network, building on delivering solutions that are responsible, safe, and sustainable." />
+        <meta name="twitter:description" content="RAS, a Goodrich company, focuses on supply chain continuity through a well connected agency network, building on delivering solutions that are responsible, safe, and sustainable." />
+        <meta name="twitter:image" content="/images/twitter_image.png" />
       </Head>
       <body>
         <Main />
